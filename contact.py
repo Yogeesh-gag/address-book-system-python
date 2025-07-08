2e37d586da847fe0bc39ab796aaa64d6f168da9d
Yogi@gag <dr.yogeesh31@gmail.com>
2025-07-08 11:21:25 +0530

    [Yogeesh]feat:Added the contact class to add the person

---
+++ contact.py
@@ -0,0 +1,12 @@
+
+class Contact:
+
+    def __init__(self, first_name, last_name, address,city, state, zip_code,phone_number,email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.phone_number = phone_number
+        self.email = email
